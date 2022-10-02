6b973ab2fa2a68a57815e4d2a95bbcecb02c34d4
Michael <michaelshabatin@mail.ru>
2022-10-02 23:08:18 +0500

    added type guard for error handling

--- src/utils/calcCalculation.ts
+++ src/utils/calcCalculation.ts
@@ -1,12 +1,17 @@
 import { calcCount } from "./calcCount";
 import { calcParser } from "./calcParser";
+import { isHandledError } from "./typeGuardError";
 
 export const calcCalculation = (expression: string) => {
   try {
     const [numberObjects, signs] = calcParser(expression);
     const evaluation = calcCount(numberObjects, signs);
     return evaluation;
-  } catch (error: any) {
-    return error.message;
+  } catch (error) {
+    if (isHandledError(error)) {
+      return error.message;
+    } else {
+      return "Unknown error!";
+    }
   }
 };
---
+++ src/utils/typeGuardError.ts
@@ -0,0 +1,3 @@
+export const isHandledError = (value: any): value is Error => {
+  return value.hasOwnProperty("message");
+};
